84114b5c09deb01608ac31c2e973c8bac3ae18df
Malcolm Kiano <malcolmkiano@gmail.com>
2020-04-21 17:09:37 -0400

    completed search functionality

--- src/routes/dashboard/tabs/search/search.js
+++ src/routes/dashboard/tabs/search/search.js
@@ -10,8 +10,38 @@ import AppContext from '../../dashboard-context'
 class Search extends Component {
   static contextType = AppContext
 
+  state = {
+    results: []
+  }
+
+  performSearch = () => {
+    const query = this.props.location && queryString.parse(this.props.location.search).q
+    const { entries } = this.context
+    this.setState({
+      query: query,
+      results: query ? EntryService.search(query, entries) : []
+    })
+  }
+
   componentDidMount() {
     document.title = 'Search - Shajara - Journal App'
+    this.performSearch()
+  }
+
+  componentDidUpdate(props) {
+    if ((props.entries !== this.props.entries) ||
+      (props.location && props.location.search !== this.props.location.search)) {
+      this.performSearch()
+    }
+  }
+
+  handleUpdate = e => {
+    const query = e.target.value
+
+    this.props.history.push({
+      pathname: '/dashboard/search',
+      search: query && '?q=' + query
+    })
   }
 
   handleUpdate = e => {
@@ -24,38 +54,22 @@ class Search extends Component {
   }
 
   render() {
-    const query = this.props.location && queryString.parse(this.props.location.search).q
-    const { entries } = this.context
-    const results = query ? EntryService.search(query, entries) : []
-    const months = results.map(([month, entries]) => {
-      const list = entries.map(entry => EntryService.makeComponent(entry))
-      return (
-        <div key={month} className="month">
-          <EntryList title={month} entries={list} />
-        </div>
-      )
-    })
+    const { query, results } = this.state
+    const list = results.map(entry => EntryService.makeComponent(entry))
 
     return (
-      <form className="wrapper search-tab">
+      <form className="wrapper search">
         <h2>Search</h2>
         <div className="search-field">
-          <Input
-            value={query}
-            onChange={this.handleUpdate}
-            label="Query"
-            placeholder="Query"
-            autoFocus={true} />
-            
+          <Input value={query} onChange={this.handleUpdate} label="Query" autoFocus={true} />
           <Button type="search" variant="alt" disabled={true} />
         </div>
-        {results.length
-          ? months
-          : (
-            <EntryList
-              EmptyImage={SearchImage}
-              showText={query ? 'No entries matched your query' : 'Search for entries using keywords'} />
-          )}
+        <ul className="results">
+          <EntryList
+            entries={list}
+            EmptyImage={SearchImage}
+            showText={query ? 'No entries matched your query' : 'Search for entries using keywords'} />
+        </ul>
       </form>
     )
   }
